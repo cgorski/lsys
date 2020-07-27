e5b71d5c5379f4dfce705f862c5cf48a0ff57100
Chris Gorski <chris.gorski@airtime.com>
2020-07-27 15:16:23 -0400

    test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,7 @@ tree2root = createRoot['0'] (createMatchFunc tree2)
 
 treeGenSymbols0Expected = "0"
 treeGenSymbols1Expected = "1[0]0"
-treeGenSymbols2Excepted = "11[1[0]0]1[0]0"
+treeGenSymbols2Expected = "11[1[0]0]1[0]0"
 
 main :: IO ()
 main =
@@ -14,3 +14,7 @@ main =
       describe "Lib.genSymbols (BinaryTree)" $ do
         it "generates generation 0 of a tree" $ do
           (genSymbols tree2root 0) `shouldBe` treeGenSymbols0Expected
+        it "generates generation 1 of a tree" $ do
+          (genSymbols tree2root 1) `shouldBe` treeGenSymbols1Expected
+        it "generates generation 2 of a tree" $ do
+          (genSymbols tree2root 2) `shouldBe` treeGenSymbols2Expected
